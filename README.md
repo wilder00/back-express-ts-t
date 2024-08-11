ecc0664b03c515559ae3040bb184372ab957fdb4
Wilder Trujillo <nadie437@gmail.com>
2024-08-11 17:40:58 -0500

    feat: add initial test config, eslint config and prettier config

---
+++ README.md
@@ -0,0 +1,3 @@
+# TEMPLATE EXPRESS TS PROJECT
+
+A template or base project which use express with typescript
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,21 @@
-import express, { Request, Response } from "express";
-import dotenv from "dotenv";
+import express, { Request, Response } from 'express'
+import dotenv from 'dotenv'
 
 // configures dotenv to work in your application
-dotenv.config();
-const app = express();
+dotenv.config()
+const app = express()
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 
-app.get("/", (request: Request, response: Response) => { 
-  response.status(200).send("Hello World");
-}); 
+app.get('/', (request: Request, response: Response) => {
+  response.status(200).send('Hello World')
+})
 
-app.listen(PORT, () => {
-  console.log("Server running at PORT: ", PORT); 
-}).on("error", (error) => {
-  // gracefully handle error
-  throw new Error(error.message);
-});
+app
+  .listen(PORT, () => {
+    console.log('Server running at PORT: ', PORT)
+  })
+  .on('error', (error) => {
+    // gracefully handle error
+    throw new Error(error.message)
+  })
